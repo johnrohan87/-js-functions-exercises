c1561bfb04b139ae09ea64ae073a3305375cfe22
John Rohan <john.rohan87@gmail.com>
2020-05-30 01:54:09 +0000

    updating

--- exercises/05-Defining-vs-Calling-a-function/app.js
+++ exercises/05-Defining-vs-Calling-a-function/app.js
@@ -1,5 +1,6 @@
 //Define the function called "multi" that expects 2 parameters:
 
+function multi(numOne,numTwo) {return numOne*numTwo;};
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
