17d77880136ad10d61f8bc22b4bf77dee1134f02
John Rohan <john.rohan87@gmail.com>
2020-05-30 01:49:29 +0000

    updating

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -7,5 +7,12 @@ function calculateArea(length,edge)
 
 
 
-var squareArea = calculateArea(3,6)
+let squareArea = calculateArea(3,6);
 
+let squareArea2 = functionCall(4,4);
+
+let squareArea3 = functionCall(2,2);
+
+let squareArea4 = functionCall(5,5);
+
+console.log(squareArea);
